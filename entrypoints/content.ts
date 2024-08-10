003de651d2d586c46b3f6fe445deb5f9a019d91f
aguywhosaguy <caseypeg8@gmail.com>
2024-08-10 17:43:11 -0400

    feat: added popup inject functionality

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -2,7 +2,12 @@ export default defineContentScript({
   matches: ['*://*.thirtydollar.website/*'],
   async main() {
     const button = await (await fetch(browser.runtime.getURL('/button.html'))).text();
-    const div = document.getElementById('main')?.children[9]
-    div?.insertAdjacentHTML('afterbegin', button)
+    const popup = await (await fetch(browser.runtime.getURL('/popup.html'))).text();
+
+    const buttonDiv = document.getElementById('main')?.children[9]
+    const popupDiv = document.getElementById('everything')
+
+    buttonDiv?.insertAdjacentHTML('afterbegin', button)
+    popupDiv?.insertAdjacentHTML('beforeend', popup)
   },
 });
