c6dd540fef201b6e710ff11d5270436370c6f3e5
aguywhosaguy <caseypeg8@gmail.com>
2024-08-11 14:13:13 -0400

    feat: finished popup frontend

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -14,5 +14,34 @@ export default defineContentScript({
     popupDiv?.insertAdjacentHTML('beforeend', popup)
 
     // hook
+    const audioText = document.getElementById('audioFileName')
+    const iconText = document.getElementById('iconFileName')
+
+    const iconInput = document.getElementById('iconInput')
+    const audioInput = document.getElementById('audioInput')
+
+    const listener = (e: Event) => {
+        const fileInput = e.target
+        if (fileInput.files && fileInput.files.length > 0) {
+        // Get the name of the selected file
+        const fileName = fileInput.files[0].name;
+        // Update the <p> tag with the selected file name
+        const fileNameDisplay = fileInput.nextElementSibling
+        fileNameDisplay.textContent = fileName;
+      } else {
+        // Reset the <p> tag if no file is selected
+        fileNameDisplay.textContent = 'No file selected';
+      }
+    }
+
+    iconInput.addEventListener('change', listener)
+    audioInput.addEventListener('change', listener)
+
+    const form = document.getElementById('fileForm') as HTMLFormElement
+    form.addEventListener('submit', function(event: Event) {
+        event.preventDefault()
+
+        console.log(new FormData(form))
+    })
   },
 });
